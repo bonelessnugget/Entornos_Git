6879dd90519153cf2feddd360ce76016d47ba244
bonelessnugget <yszeedyt99@gmail.com>
2021-12-01 12:35:40 +0100

    modificado

--- pruebagit.java
+++ pruebagit.java
@@ -1,1 +1,1 @@
-public calss Main {	public static void main(String[] args) {		System.out.println("a");}}
+akjsdkdaflskdjafpublic calss Main {	public static void main(String[] args) {		System.out.println("a");}}
